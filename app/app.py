7bed99c37ba77901ee86daf792f35ba7d32a51c5
Dmitriy Piskarev <51864090+DmitryPiskarev@users.noreply.github.com>
2025-10-15 10:41:31 -0700

    Add margin to divider in app.py

--- app/app.py
+++ app/app.py
@@ -90,7 +90,7 @@ def index() -> rx.Component:
                     class_name="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start",
                 ),
                 rx.el.div(class_name="py-4 lg:py-6"),
-                rx.divider(),
+                rx.divider(margin_bottom="1em"),
                 rx.el.div(
                     expenses_input(),
                     budget_output(),
